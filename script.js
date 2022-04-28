09f2b60988b4da7f979a48728cc06ff9955dd72c
AdrienG88 <78536627+AdrienG88@users.noreply.github.com>
2022-04-28 19:36:18 +0200

    For Alex to debug

--- script.js
+++ script.js
@@ -1,73 +1,84 @@
-/* function load_best_movie(url)
-{
-	load_json(url).then(json =>
-	{
-		load_json(json.results[0]["id"]).then(sub_json =>
-		{
-			document.getElementById("best-movie").innerHTML = 
-			`<div id="information-best-movie">
-				<h1 id="title-best-movie">${json.results[0]["title"]}</h1>
-				<button id="button-play" onclick="show_modal(${json.results[0]["id"]})">Play</button>
-				<p id="description-best-movie">${sub_json["long-description"]}</p>
-			</div>
-			<div id="img-best-movie">
-				<img src="${json.results[0]["image_url"]}" id="img_movie_0" width="450" height="550">
-			</div>`;
+const URLBASE = "http://localhost:8000/api/v1/titles/"
+let dynamicUrl = "?sort_by=-imdb_score"
+let url = URLBASE + dynamicUrl
+const myImage = document.getElementById("best-rated-movie-slide1-img");
+const myRequest = new Request(url)
+let urlPage2 = url + "&page=2";
+let myArray = [];
 
-		})
-	})
+
+async function test(url)
+{
+    const response = await fetch(url);
+    const result = await response.json();
+    return result;
 }
+
+
+async function call()
+{
+    const data = await test(url);
+
+    for (row of data.results)
+    {
+        myArray.push(row);
+    }
+    console.log(myArray);
+
+    // faire une boucle !
+    const dataPage2 = await test(urlPage2);
+    myArray.push(dataPage2.results[0]);
+    myArray.push(dataPage2.results[1]);
+
+    console.log(myArray);
+    return await Promise.all(myArray);
+}
+
+function fillData(myArray)
+{
+    for (let i = 0; i < 7; i++)
+    {
+        document.getElementById(`best-rated-movie-slide${i+1}-title`).innerHTML = `${myArray[i].title}`;
+        document.getElementById(`best-rated-movie-slide${i+1}-img`).src = `${myArray[i].image_url}`;
+
+
+
+        document.getElementById(`best-rated-movie-slide${i+1}-modal-heading`).innerHTML = "Title: " + `${myArray[i].title}`;
+
+        let genreList = myArray[i].genres;
+        console.log(genreList);
+        let genreString = "";
+        for (row of genreList)
+        {
+            genreString+= row + ",";
+        }
+        if (genreString.length > 0)
+        {
+            genreString.slice(-1);
+        }
+        else
+        {
+            genreString = "Not Specified";
+        }
+
+
+        console.log("genreList:", genreList);
+        console.log("genreString: ", genreString);
+
+        document.getElementById(`best-rated-movie-slide${i+1}-modal-genre`).innerHTML = "Genre: " + genreString;
+        document.getElementById(`best-rated-movie-slide${i+1}-modal-releasedate`).innerHTML = "Year: " + `${myArray[i].year}`;
+        document.getElementById(`best-rated-movie-slide${i+1}-modal-votes`).innerHTML = "Votes: " + `${myArray[i].votes}`;
+        document.getElementById(`best-rated-movie-slide${i+1}-modal-imdbscore`).innerHTML = "IMDb score: " + `${myArray[i].imdb_score}`;
+/*
+        //document.getElementById(`best-rated-movie-slide${i+1}-modal-directors`).innerHTML = `${myArray[i].directors}`;
+        //document.getElementById(`best-rated-movie-slide${i+1}-modal-actors`).innerHTML = `${myArray[i].actors}`;
+        //document.getElementById(`best-rated-movie-slide${i+1}-modal-duration`).innerHTML = `${myArray[i].duration}`;
+        //document.getElementById(`best-rated-movie-slide${i+1}-modal-country`).innerHTML = `${myArray[i].country}`;
+        //document.getElementById(`best-rated-movie-slide${i+1}-modal-boxoffice`).innerHTML = `${myArray[i].box_office}`;
+        //document.getElementById(`best-rated-movie-slide${i+1}-modal-resume`).innerHTML = `${myArray[i].resume}`;
 */
-
-function loadBestMovie(url)
-{
-	loadJson(url).then(function (json)
-	{
-		loadJson(json.results[0]["id"]).then(function (subJson)
-		{
-			document.getElementById("best-rated-movie-slide1-title").innerHTML =
-			`${json.results[0]["title"]}`;
-
-			document.getElementById("best-rated-movie-slide1-img").src =
-			`${json.results[0]["image_url"]}`;
-
-			document.getElementById("best-rated-movie-slide1-description").innerHTML=
-			`Titre: ${json.results[0]["title"]} <br>
-			Genre: ${json.results[0]["genres"]} <br>
-			Année de sortie: ${json.results[0]["year"]} <br>
-			Score Imdb: ${json.results[0]["imdb_score"]} <br>
-			Réalisateur(s): ${json.results[0]["directors"]} <br>
-			Acteurs: ${json.results[0]["actors"]}` ;
-
-
-		})
-	})
+    }
 }
 
-// Durée: requête avec lien page imdb + getElementsByClass("ipc-inline-list__item").innerHTML
-
-function loadJson(url)
-{
-	try
-	{
-		return fetch(URL_BASE+url).then(function (response)
-		{
-			if (response.ok)
-			{
-				return response.json();
-			}
-			else
-			{
-				throw new Error("La requete a echoue.");
-			}
-		})
-	}
-	catch(e)
-	{
-		console.log(e.message)
-	}
-}
-
-const URL_BASE = "http://localhost:8000/api/v1/titles/"
-let url = "?title=Alien"
-loadBestMovie(url)
+let a = Promise.resolve(call());
+fillData(a)
